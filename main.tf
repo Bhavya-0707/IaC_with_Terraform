cdfa1e6aa7f0b9f5f2759a1d4eb7d4064f8c09c7
Bhavya-0707 <bhavyasri.ponaganti@gmail.com>
2025-09-25 11:03:23 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+# Pull the latest Nginx image
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = true
+}
+
+# Run a container using the image
+resource "docker_container" "nginx" {
+  name  = "my-nginx"
+  image = docker_image.nginx.image_id
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
